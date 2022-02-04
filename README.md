62b751dfc78890c758b663ffbcdd9628bf87f938
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2022-02-04 16:44:38 +0000

    test js and delete files

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # vef2-v1
 
 Verkefni 1 - Vefforritun II - 2022
+
+# npm install 
+# npm run test
+# npm run build 
+# npm run lint
--- test/makehtml.test.js
+++ test/makehtml.test.js
@@ -1,2 +1,39 @@
-import { describe, expect, it } from "@jest/globals";
-import { blogTemplate, makeHTML } from "./make-html";
+import { describe, expect, it } from '@jest/globals';
+import { makeTemplate, makeList } from '../src/make-html';
+
+describe('html', () => {
+  it('creates a html string', () => {
+    const input = {
+      content: '<strong>hi</strong>',
+      metadata: {
+        date: 'DATE',
+      },
+    };
+
+    const parsed = makeList(input);
+
+    const output = `
+    <section>
+      <strong>hi</strong>
+      <p>Skrifað: DATE</p>
+    </section>
+  `;
+    expect(parsed).toBe(output);
+  });
+
+  it.skip('creates a html template', () => {
+    const parsed = makeTemplate('');
+
+    const output = `
+  <!doctype html>
+  <html>
+    <head>
+      <title></title>
+      <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+    </body>
+  </html>`;
+    expect(parsed).toBe(output);
+  });
+});
